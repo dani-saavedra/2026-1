d2dc121a298066064566a9123d12e1ecb63c2638
Daniel Saavedra Fonnegra <daniel.saavedra.fon@gmail.com>
2026-02-19 19:09:51 -0500

    Fix syntax error in sumar method

--- src/main/java/com/example/demo/Calculadora.java
+++ src/main/java/com/example/demo/Calculadora.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 public class Calculadora {
 
-    public int sumar(int numero1, int numero2) {
+    public int sumar(int numero1, int numero2) 
         return numero1 + numero2;
     }
 }
