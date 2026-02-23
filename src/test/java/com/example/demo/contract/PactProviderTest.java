736100b0e2466abeb8e6e93b83972d6a82acdc8b
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2026-02-23 08:34:17 -0500

    Comment pact

--- src/test/java/com/example/demo/contract/PactProviderTest.java
+++ src/test/java/com/example/demo/contract/PactProviderTest.java
@@ -12,31 +12,31 @@ import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@ActiveProfiles("test")
-@Provider("XyzService")
-@PactBroker(url = "http://localhost:9292")
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+//@ActiveProfiles("test")
+//@Provider("XyzService")
+//@PactBroker(url = "http://localhost:9292")
 public class PactProviderTest {
 
-    @MockitoBean
+    @MockBean
     private UserService userService;
 
-    @TestTemplate
-    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    //@TestTemplate
+  //  @ExtendWith(PactVerificationInvocationContextProvider.class)
     void pactVerificationTestTemplate(PactVerificationContext context) {
         context.verifyInteraction();
     }
 
-    @BeforeEach
+//    @BeforeEach
     void setupTestTarget(PactVerificationContext context) {
         System.setProperty("pact.verifier.publishResults", "true");
         System.setProperty("pact.provider.version", "1");
     }
 
-    @State("El usuario con ID 1 existe")
+  //  @State("El usuario con ID 1 existe")
     public void userExits() {
         Mockito.when(userService.consultarUsuario(Mockito.any()))
                 .thenReturn(new User(1L, "Juan", "Perez", "juan@example.com"));
